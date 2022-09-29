3ff883c8253126f556a3192ab3e193a2c44051f2
kshitij3pg <112077506+kshitij3pg@users.noreply.github.com>
2022-09-29 13:30:11 +0530

    Update loginPage.java

--- src/test/java/pages/loginPage.java
+++ src/test/java/pages/loginPage.java
@@ -1,29 +1,25 @@
 package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class loginPage {
 	WebDriver driver;
-
-	By email=By.id("txtUsername");
-	By password= By.id("txtPassword");
-	By login=By.xpath("//div//button");
+	By userName=By.id("txtUsername");
+	By pswd= By.id("txtPassword");
+	By loginBtn=By.xpath("//div//button");
 	public loginPage(WebDriver driver) {
 		this.driver= driver;
-		System.out.println(driver.getTitle());
-		
+		System.out.println(driver.getTitle());	
 	}
 	
-
 	public dashboardPage login(String user, String pass) throws InterruptedException{
-		 WebDriverWait wait = new WebDriverWait(driver,2);
-		  WebElement element = wait.until(ExpectedConditions.visibilityOf(driver.findElement(email)));
-		driver.findElement(email).sendKeys(user);
-		driver.findElement(password).sendKeys(pass);
-		driver.findElement(login).click();
+		WebDriverWait wait = new WebDriverWait(driver,2);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(userName)));
+		driver.findElement(userName).sendKeys(user);
+		driver.findElement(pswd).sendKeys(pass);
+		driver.findElement(loginBtn).click();
 		Thread.sleep(3000);
 		return new dashboardPage(driver);
 	}
